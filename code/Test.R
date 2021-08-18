23d9a9a88857759702eeb6bb299cdc98af6b66a0
kwolf3 <kmwolf3@illinois.edu>
2021-08-18 13:50:06 -0500

    Install packages and start coding

---
+++ code/Test.R
@@ -0,0 +1,5 @@
+rm(list = ls())
+library(magrittr)
+search()
+
+chicago_df <- read.csv('/Users/kerstinwolf/DS Projects/GitHub/US_City_Report_Generator_Project/data/city_weather_data/chicago_data.csv', header = TRUE)
