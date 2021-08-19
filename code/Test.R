5d7bc2d80a4477d2822841b0be4beb7e8e22fcf0
kwolf3 <kmwolf3@illinois.edu>
2021-08-19 12:14:50 -0500

    Add comments

--- code/Test.R
+++ code/Test.R
@@ -2,8 +2,10 @@ rm(list = ls())
 library(magrittr)
 search()
 
+# original Chicago dataa
 chicago_df <- read.csv('/Users/kerstinwolf/DS Projects/GitHub/US_City_Report_Generator_Project/data/city_weather_data/chicago_data.csv', header = TRUE)
 
+# clean up the data
 df_clean <- chicago_df %>%
   dplyr::mutate(datetime = paste(Date, Time)) %>%
   dplyr::mutate(datetime = strptime(datetime, format = '%m/%d/%y %I:%M %p')) %>%
@@ -15,5 +17,19 @@ df_clean <- chicago_df %>%
   dplyr::mutate(Pressure_in = gsub('in', '', Pressure_in)) %>%
   dplyr::mutate(Precipitation_in = gsub('in', '', Precipitation_in))
 
+# the clean data reorganized in a way so that I can easily work with it
 df <- df_clean %>%
   dplyr::select(datetime, temp = Temperature_F, dewpoint = DewPoint_F, humidity = Humidity_Percentage, wind = Wind, windspeed = WindSpeed_mph, windgust = WindGust_mph, pressure = Pressure_in, precip = Precipitation_in, condition = Condition, loc = Location, Station)
+
+
+
+
+
+
+
+
+
+
+
+
+
