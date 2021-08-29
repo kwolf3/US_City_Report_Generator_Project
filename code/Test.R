dd51e0e72c029d44cac7bfcd56ff179b0f8b98f3
kwolf3 <kmwolf3@illinois.edu>
2021-08-29 11:31:45 -0500

    Adjust columns in df_clean to numeric

--- code/Test.R
+++ code/Test.R
@@ -14,13 +14,14 @@ a_df2 <- read.csv('/Users/kerstinwolf/DS Projects/GitHub/US_City_Report_Generato
 df_clean <- chicago_df %>%
   dplyr::mutate(datetime = paste(Date, Time)) %>%
   dplyr::mutate(datetime = strptime(datetime, format = '%m/%d/%y %I:%M %p')) %>%
-  dplyr::mutate(Temperature_F = gsub('°F', '', Temperature_F)) %>%
-  dplyr::mutate(DewPoint_F = gsub('°F', '', DewPoint_F)) %>%
-  dplyr::mutate(Humidity_Percentage = gsub('%', '', Humidity_Percentage)) %>%
-  dplyr::mutate(WindSpeed_mph = gsub('mph', '', WindSpeed_mph)) %>%
-  dplyr::mutate(WindGust_mph = gsub('mph', '', WindGust_mph)) %>%
-  dplyr::mutate(Pressure_in = gsub('in', '', Pressure_in)) %>%
-  dplyr::mutate(Precipitation_in = gsub('in', '', Precipitation_in))
+  dplyr::mutate(Temperature_F = as.numeric(stringr::str_trim(stringr::str_remove(Temperature_F, '°F')))) %>%
+  #dplyr::mutate(Temperature_F = gsub('°F', '', Temperature_F)) %>%
+  dplyr::mutate(DewPoint_F = as.numeric(stringr::str_trim(stringr::str_remove(DewPoint_F,'°F')))) %>% 
+  dplyr::mutate(Humidity_Percentage = as.numeric(stringr::str_trim(stringr::str_remove(Humidity_Percentage, '%')))) %>% #gsub('%', '', Humidity_Percentage)) %>%
+  dplyr::mutate(WindSpeed_mph = as.numeric(stringr::str_trim(stringr::str_remove(WindSpeed_mph, 'mph')))) %>% #gsub('mph', '', WindSpeed_mph)) %>%
+  dplyr::mutate(WindGust_mph = as.numeric(stringr::str_trim(stringr::str_remove(WindGust_mph, 'mph')))) %>% #gsub('mph', '', WindGust_mph)) %>%
+  dplyr::mutate(Pressure_in = as.numeric(stringr::str_trim(stringr::str_remove(Pressure_in, 'in')))) %>% #gsub('in', '', Pressure_in)) %>%
+  dplyr::mutate(Precipitation_in = as.numeric(stringr::str_trim(stringr::str_remove(Precipitation_in, 'in')))) #gsub('in', '', Precipitation_in))
 
 # the clean data reorganized in a way so that I can easily work with it
 df <- df_clean %>%
@@ -30,7 +31,7 @@ df_loc <- a_df2 %>%
   dplyr::select(station = Station, lat = Lat, long = Long) %>%
   dplyr::slice(grep(as.character(df$station[1]), station))
 
-grep(df$station[1], df_loc$station)
+#grep(df$station[1], df_loc$station)
 
 # dataframe max and min temp of each day
 df_temp <- df %>%
@@ -39,6 +40,13 @@ df_temp <- df %>%
   dplyr::summarize(min_temp = min(temp),
                    max_temp = max(temp))
   
+# datafrmae of average temp each day
+df_av_temp <- df %>%
+  dplyr::select(datetime, temp) %>%
+  dplyr::group_by(date = as.Date(datetime)) %>%
+  dplyr::summarize(av_temp = mean(temp))
+
+
 #####################
 
 # All tables
@@ -62,6 +70,11 @@ df_temp %>%
   theme(legend.margin = margin(0, 0, 0, 0),
         legend.key.size = unit(0.5, 'cm')) +
   scale_color_manual(values = c('red' = 'red', 'blue' = 'blue'), labels = c('Maximum Temperature', 'Minimum Temperature'))
+
+# total precipitation graph
+
+
+# average temp graph
 
 
 
